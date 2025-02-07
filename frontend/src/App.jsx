7646ef4e73e0cd153735fb998c79fe6f7a1daa83
taher <mindjobs994@gmail.com>
2025-02-07 19:47:47 +0530

    added themes

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ThemeButton from './components/themeButton'
+
+const App = () => {
+  return (
+    <div>
+      <div className="toast">
+        <ThemeButton/>
+      </div>
+    </div>
+  )
+}
+
+export default App
